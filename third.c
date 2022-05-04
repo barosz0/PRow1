e5d42a77e9787b1b0fbef8be0d07cb0a23f6722a
barosz0 <barosz0@wp.pl>
2022-05-04 14:26:46 +0200

    Naprawa problemu z widocznością zmiennych.

--- third.c
+++ third.c
@@ -8,52 +8,51 @@
 
 int main(int argc, char* argv[])
 {
-	clock_t spstart, spstop,ppstart,ppstop;
-	
-    int n = 0, m = 1000000;
+    clock_t spstart, spstop, ppstart, ppstop;
 
-	double sswtime, sewtime;
-//volatile
+    int n = 0, m = 10000;
 
-    
+    double sswtime, sewtime;
+    //volatile
+
     char* isprime = (char*)calloc(m, sizeof(char));
 
-    //memset(isprime,0,sizeof(isprime));
-    isprime[0]=1;
-    isprime[1]=1;
+    //memset(isprime, 0, sizeof(isprime));
+    isprime[0] = 1;
+    isprime[1] = 1;
 
 
-	sswtime = omp_get_wtime();
-	spstart = clock();
+    sswtime = omp_get_wtime();
+    spstart = clock();
 
-    int i =0, id =0;
-    omp_set_num_threads(4);
-    #pragma omp parallel for schedule(static)
-    for(id=0;id<omp_get_num_threads();id++)
+    int id;
+    omp_set_num_threads(8);
+#pragma omp parallel for schedule(static, 1)
+    for ( id = 0; id < omp_get_num_threads(); id++)
     {
-        int slice = m/omp_get_num_threads();
+        int slice = m / omp_get_num_threads();
 
-        int from = id*slice;
-        int to = from+slice;
-        if(to>m)
+        int from = id * slice;
+        int to = from + slice;
+        if (to > m)
             to = m;
-
+        printf("%d: %d %d\n", id, from, to);
         // int to = m - i*slice;
         // int from = to - slice;
 
         // if (from < 0)
         //     from = 0;
-        
 
 
-        for(i=0;i<to;i++)
+
+        for (int i = 0; i < to; i++)
         {
-            if(!isprime[i])
+            if (!isprime[i])
             {
-                for(int j = i*2; j<m; j+=i)
+                for (int j = i * 2; j < m; j += i)
                 {
-                    if(j>=from)
-                        isprime[j]=1;
+                    if (j >= from)
+                        isprime[j] = 1;
                 }
             }
 
@@ -62,18 +61,20 @@ int main(int argc, char* argv[])
     }
 
 
-	spstop = clock();
+    spstop = clock();
     sewtime = omp_get_wtime();
-    
+
     int prime_nums = 0;
 
-    for(int i = n;i<m;i++){
-        if(!isprime[i])
+    for (int i = 0; i < m; i++) {
+        if (!isprime[i]) {
+            printf("%d, ", i);
             prime_nums++;
+        }
     }
-    printf("%d \n",prime_nums);
+    printf("\n%d \n", prime_nums);
 
-	printf("Czas procesorów przetwarzania rownoleglego  %f sekund \n", ((double)(spstop - spstart)/CLOCKS_PER_SEC));
-	printf("Czas trwania obliczen rownoleglego - wallclock %f sekund \n",  sewtime-sswtime);
-	return 0;
+    printf("Czas procesor�w przetwarzania rownoleglego  %f sekund \n", ((double)(spstop - spstart) / CLOCKS_PER_SEC));
+    printf("Czas trwania obliczen rownoleglego - wallclock %f sekund \n", sewtime - sswtime);
+    return 0;
 }
